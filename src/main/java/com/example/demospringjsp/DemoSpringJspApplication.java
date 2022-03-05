fd7b7c4fa2d3db3775c6bf76c7b8edb111b28a78
jinwoo park <swat018@gmail.com>
2022-03-05 21:32:34 +0900

    Initial commit

---
+++ src/main/java/com/example/demospringjsp/DemoSpringJspApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringJspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demospringjsp/DemoSpringJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
